09c4017c0328f64e90dc6993840e6fda45db7b7e
juliaobenauer <julia.obenauer@boehringer-ingelheim.com>
2022-05-14 06:11:27 +0200

    addded "ON CONFLICT" statement to user_table_insert

--- sql_queries.py
+++ sql_queries.py
@@ -71,9 +71,13 @@ INSERT INTO songplays (songplay_id, start_time, user_id, level, song_id, artist_
 VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
 """)
 
+# user_id(73) excluded due to conflict
 user_table_insert = ("""
 INSERT INTO users (user_id, first_name, last_name, gender, level)
 VALUES (%s, %s, %s, %s, %s)
+ON CONFLICT (user_id)
+DO UPDATE
+    SET level = EXCLUDED.level
 """)
 
 song_table_insert = ("""
@@ -95,7 +99,7 @@ VALUES (%s, %s, %s, %s, %s, %s, %s)
 # FIND SONGS
 
 song_select = ("""
-SELECT song_id, songs.artist_id 
+SELECT song_id, s.artist_id 
 FROM songs AS s
 JOIN artists AS a
 ON a.artist_id = s.artist_id
